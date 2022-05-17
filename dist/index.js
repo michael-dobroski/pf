644386f6951b6e8ac64b871c19207962950dc4c4
Michael Dobroski <michael-dobroski@outlook.com>
2022-05-17 15:08:28 -0500

    started adding text

--- dist/index.js
+++ dist/index.js
@@ -2,38 +2,23 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.120.1/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'https://unpkg.com/three@0.120.1/src/loaders/FontLoader.js'
 
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene()
+const canvas = document.querySelector('.webgl');
+const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-// init
-
-const sizes = {
-    width: window.innerWidth,
-    height:window.innerHeight
-}
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 1000)
-camera.position.set(100,0,100)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0,10,0);
+scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
+});
 
-const controls = new OrbitControls( camera, renderer.domElement )
+const controls = new OrbitControls( camera, renderer.domElement );
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.shadowMap.enabled = true
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+renderer.shadowMap.enabled = true;
 
-// Loading images into VR environment
 let materialArray = [];
 let texture_ft = new THREE.TextureLoader().load( 'front.jpg' );
 let texture_bk = new THREE.TextureLoader().load( 'back.jpg' );
@@ -41,8 +26,6 @@ let texture_up = new THREE.TextureLoader().load( 'top.jpg' );
 let texture_dn = new THREE.TextureLoader().load( 'bot.jpg' );
 let texture_rt = new THREE.TextureLoader().load( 'left.jpg' );
 let texture_lf = new THREE.TextureLoader().load( 'right.jpg' );
-
-//Overlapping mesh objects with textures  
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
@@ -50,19 +33,46 @@ materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
    
-for (let i = 0; i < 6; i++)
-  materialArray[i].side = THREE.BackSide;
+for (let i = 0; i < 6; i++) {
+    materialArray[i].side = THREE.BackSide;
+}
    
 let skyboxGeo = new THREE.BoxGeometry( 1000, 1000, 1000);
 let skybox = new THREE.Mesh( skyboxGeo, materialArray );
 scene.add( skybox );
 
-const loader = new FontLoader();
+addText('Welcome to my portfolio!', 20, Math.PI * (3/2), 0, 0, 0, -400, -30);
+addText('Look up and take a look around.', 20, Math.PI * (3/2), 0, 0, 0, -400, 0);
+addText('Thanks, enjoy!', 20, Math.PI * (3/2), 0, 0, 0, -400, 30);
 
-function animate() {
-    requestAnimationFrame(animate)
-    controls.update()
-    renderer.render(scene, camera)
+function addText(txt, fontSize, rotX, rotY, rotZ, x, y, z) {
+    const loader = new FontLoader();
+    loader.load( 'https://unpkg.com/three@0.120.1/examples/fonts/helvetiker_regular.typeface.json' , function ( font ) {
+
+        const textGeo = new THREE.TextGeometry( txt, {
+            font: font,
+            size: fontSize,
+            height: 5,
+            curveSegments: 12,
+        } );
+        textGeo.rotateX(rotX);
+        textGeo.rotateY(rotY);
+        textGeo.rotateZ(rotZ);
+        const material = new THREE.MeshBasicMaterial({
+            color: 0xffffff
+        });
+        const mesh = new THREE.Mesh(textGeo, material);
+        scene.add(mesh);
+        mesh.geometry.center();
+        mesh.position.set(x, y, z);
+
+    } );
 }
 
-animate()
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
